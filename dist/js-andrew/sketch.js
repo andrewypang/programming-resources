90093bf71bfd6d8510dde0135596681c28fb26df
andrewypang <40625083+andrewypang@users.noreply.github.com>
2019-10-15 13:16:58 -0700

    hello world

---
+++ dist/js-andrew/sketch.js
@@ -0,0 +1,14 @@
+// INITIALIZE COLLAPSIBLE
+$(document).ready(function () {
+    $('.collapsible').collapsible();
+});
+
+// INITIALIZE SCROLLSPY
+$(document).ready(function () {
+    $('.scrollspy').scrollSpy();
+});
+
+// INITIALIZE MOBILE COLLAPSE
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
